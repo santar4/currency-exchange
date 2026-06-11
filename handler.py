86b4b0592e074da4a24aa5ed215e878e61ce1fd0
santar4 <mdykovv@gmail.com>
2026-06-11 22:32:40 +0300

    start_command

--- handler.py
+++ handler.py
@@ -7,9 +7,15 @@ from settings import form_router
 
 router = Router()
 
-@form_router.message(Command('/start'))
+@form_router.message(Command("start"))
 async def start(message: Message):
-    await message.answer('Hello')
+    await message.answer(
+        f"Hello, {message.from_user.full_name}!\n\n"
+        "Welcome to Currency Exchanger.\n"
+        "I can help you check available currencies and convert money using real exchange rates.\n\n"
+        "Use /currencies to view supported currencies.\n"
+        "Use /convert to start currency conversion."
+    )
 
 
 @form_router.message(Command('currencies'))
